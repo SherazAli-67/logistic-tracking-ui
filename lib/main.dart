7c6bc5b957530d5c7971f456ad7d529777b10056
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-21 22:38:36 +0500

    Logistic Tracking - Directory Structure Set

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logistic_tracking_ui/constants/string_const.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: StringConst.appTitle,
       theme: ThemeData(
         brightness: .light
       ),
